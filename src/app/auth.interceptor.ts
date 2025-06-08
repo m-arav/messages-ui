0b308065f5b53ace11b6e2d61ce002826495ca1d
Aravind Murali <thearavindmurali@gmail.com>
2025-06-08 08:44:43 -0400

    Redirect to login on 401

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,9 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
+  const router = inject(Router);
   if (token) {
     req = req.clone({ setHeaders: { Authorization: token } });
   }
-  return next(req);
+
+  return next(req).pipe(
+    catchError(err => {
+        if (err.status == 401) {
+            router.navigate(["/login"]);
+        }
+        return throwError(()=> err);
+    })
+  );
 };
